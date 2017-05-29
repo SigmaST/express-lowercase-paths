98901e6c74034f0b5bbbb5d67f19274640384623
vanielf <vanielf@gmail.com>
2017-05-29 11:01:43 -0300

    Habilitando ignorar caminhos

--- express-lowercase-paths.js
+++ express-lowercase-paths.js
@@ -1,13 +1,29 @@
 var url = require("url")
 
-module.exports = function lowercasePaths() {
-  return function _lowercasePaths(req, res, next) {
-    if (req.path.toLowerCase() !== req.path) {
-      var parsedUrl = url.parse(req.originalUrl)
-      parsedUrl.pathname = parsedUrl.pathname.toLowerCase()
-      res.redirect(url.format(parsedUrl))
-    } else {
-      next()
+module.exports = function lowercasePaths(ignore) {
+  if(ignore){
+    return function _lowercasePaths(req, res, next) {
+      if(ignore(req)){
+        next();
+      }else{
+        if (req.path.toLowerCase() !== req.path) {
+          var parsedUrl = url.parse(req.originalUrl)
+          parsedUrl.pathname = parsedUrl.pathname.toLowerCase()
+          res.redirect(url.format(parsedUrl))
+        } else {
+          next()
+        }
+      }
+    }
+  }else{
+    return function _lowercasePaths(req, res, next) {
+      if (req.path.toLowerCase() !== req.path) {
+        var parsedUrl = url.parse(req.originalUrl)
+        parsedUrl.pathname = parsedUrl.pathname.toLowerCase()
+        res.redirect(url.format(parsedUrl))
+      } else {
+        next()
+      }
     }
   }
 }
